f8961e8b77e3af4c0c19530ba32414e5488254ff
Fabian Martin <martin@localdev.de>
2014-01-20 06:57:44 +0100

    mysql configuration extended

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,13 +25,30 @@ class symfony2(
       root_password => $mysql_root_password,
       restart => true,
       service_enabled => true,
+      override_options => { 'mysqld' => { 'bind-address' => '0.0.0.0' } }
     }
 
     mysql::db { 'symfony':
       user     => 'symfony',
       password => 'symfony',
-      host     => 'localhost',
+      host     => '%',
       grant    => ['ALL'],
+    }
+
+    mysql_user { 'root@%':
+      ensure                   => 'present',
+      max_connections_per_hour => '0',
+      max_queries_per_hour     => '0',
+      max_updates_per_hour     => '0',
+      max_user_connections     => '0',
+    }
+
+    mysql_grant { 'root@%/*.*':
+      ensure     => 'present',
+      options    => ['GRANT'],
+      privileges => ['ALL'],
+      table      => '*.*',
+      user       => 'root@%',
     }
   }
 
