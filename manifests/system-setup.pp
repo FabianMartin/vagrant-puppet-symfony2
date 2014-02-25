89374e0903933d14b95ca467d08b886d4e1de98b
Fabian Martin <martin@localdev.de>
2014-02-25 20:46:21 +0100

    system setup extended (copy authorized keys, cd to project on login)

--- manifests/system-setup.pp
+++ manifests/system-setup.pp
@@ -28,6 +28,32 @@ class symfony2::system-setup {
     require => User["www-data"],
   }
 
+  file { "/var/www/.ssh/":
+    ensure => directory,
+    owner => "www-data",
+    group => "www-data",
+    mode => 0700,
+    require => [User["www-data"], File["/var/www/"]],
+  }
+
+  file { "/var/www/.ssh/authorized_keys":
+    ensure => file,
+    owner => "www-data",
+    group => "www-data",
+    mode => 0644,
+    source => '/home/vagrant/.ssh/authorized_keys',
+    require => [User["www-data"], File["/var/www/"]],
+  }
+
+  file { '/var/www/.bash_login':
+    owner => "www-data",
+    group => "www-data",
+    mode => 0755,
+    ensure => file,
+    source => 'puppet:///modules/symfony2/.bash_login',
+    require => [User["www-data"], File["/var/www/"]],
+  }
+
   file { "/etc/localtime":
       ensure => "/usr/share/zoneinfo/Europe/Berlin"
   }
